617420a15b4b807e594167a948e815fcb206a817
Rahul-986 <rg4109376@gmail.com>
2024-07-26 01:20:53 +0530

    topNav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SideNav from './templates/SideNav';
+import TopNav from './templates/TopNav';
 
 const Home = () => {
   document.title="Movie App| Homepage";
@@ -8,6 +9,10 @@ const Home = () => {
    <>
 
 <SideNav/>
+<div className='w-[80%] h-full '>
+  top
+  <TopNav/>
+</div>
       
      </>
   )
---
+++ src/components/templates/TopNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TopNav = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default TopNav
